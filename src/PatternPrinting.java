a0e94dffec9160817de0428a24a1ce24b34f9b85
Neha <nehaphilip0205@gmail.com>
2026-03-18 10:00:51 +0530

    Completed Pattern Printing program

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PatternPrinting {
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter n:");
+		System.out.print();
 		int n=sc.nextInt();
 		int i;
 		for(i=0;i<n;i++)

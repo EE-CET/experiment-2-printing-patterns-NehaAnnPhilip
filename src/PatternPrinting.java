8be493eb682dcdb6714d43c926c61f2085a13618
Neha <nehaphilip0205@gmail.com>
2026-03-18 10:04:36 +0530

    Completed Pattern Printing program

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class PatternPrinting {
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		System.out.print();
 		int n=sc.nextInt();
 		int i;
-		for(i=0;i<n;i++)
+		for(i=0;i<n-1;i++)
 			System.out.print("* ");
-		System.out.println();
+		System.out.println('*');
     		for(i=0;i<n;i++)
 			System.out.println('*');
 		sc.close();
